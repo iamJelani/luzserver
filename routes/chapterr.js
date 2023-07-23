6739875f7c0346fb39b20033560d59c7284174be
iamJelani <jelani.track@gmail.com>
2023-07-23 10:16:19 +0100

    Update chapterr.js
    Added a different save logic for notes created in a Topic

--- routes/chapterr.js
+++ routes/chapterr.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { Chapter } = require("../models/chapter");
 const auth = require("../middlewere.js/auth");
 const { User } = require("../models/user");
+const { Note } = require("../models/notes");
 const chapterRouter = express.Router();
 
 chapterRouter.post("/add-chapter", async (req, res) => {
@@ -72,6 +73,56 @@ chapterRouter.post("/delete-chapter/:chapterId", auth, async (req, res) => {
   } catch (error) {
     res.json(error);
     console.log(`Failed to delete chapter: ${error}`);
+  }
+});
+
+chapterRouter.post("/create-note/chapter", auth, async (req, res) => {
+  try {
+    const { chapterId, noteTitle, noteContent, ownerId, noteId } = req.body;
+    let findNote = noteId != "" ? Note.findById(noteId) : "";
+    let topicIds = findNote ? findNote.topicIds : [];
+    let chapter = Chapter.findById(chapterId[0]);
+    if (!findNote) {
+      let note = new Note({
+        title: noteTitle,
+        topicIds: chapterId,
+        ownerId: ownerId,
+        content: noteContent,
+      });
+      await note.save();
+      if (chapter) {
+        await chapter.chapterNotes.push({ note });
+        await chapter.save();
+      } else {
+        console.log("Could not find chapter");
+      }
+      res.json("New note has been created");
+    } else {
+      const updatedNote = Note.findOneAndUpdate(
+        { _id: noteId },
+        { content: noteContent },
+        { title: noteTitle },
+        { new: true }
+      );
+
+      for (const topicId of topicIds) {
+        let chapter = Chapter.findById({ topicId });
+        let chapterNoteIndex = chapter.chapterNotes.findIndex(
+          (note) => note.note._id.toString() === noteId
+        );
+        console.log(`Note index in chapNote is: ${chapterNoteIndex}`);
+
+        if (chapterNoteIndex > -1) {
+          chapter.chapterNotes[chapter].note.title = noteTitle;
+          chapter.chapterNotes[chapter].note.content = noteContent;
+          await chapter.save();
+        }
+      }
+
+      res.json(updatedNote);
+    }
+  } catch (error) {
+    console.log(`failed to create note in chapter: ${error}`);
   }
 });
 
